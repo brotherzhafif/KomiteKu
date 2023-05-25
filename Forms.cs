398d8773a9b0b7159553010f4a40f001ea44406c
BrotherZhafif <bangz1504@gmail.com>
2023-05-25 23:06:04 +0700

    Revert "Revert "Add project files.""
    This reverts commit 7c6b2e0bc1faee87bb8b4b27e9aefb23f9df82d3.

---
+++ Forms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomiteKu
+{
+    internal class Forms
+    {
+        public static Navigation Nav_form = new Navigation();        
+    }
+}
